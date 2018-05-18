cd26daaeb21e972d24e77bc6ffa6e84960a068d8
nicogross <nicogross452@hotmail.com>
2018-05-18 13:47:03 -0300

    dias

--- segundosmain.c
+++ segundosmain.c
@@ -2,6 +2,9 @@
 
 int main(){
 	printf("Segundos restantes: %i\n", days(180000));
+	printf("segundos restantes: %i\n", horas(180000));
+	printf("segundos restantes: %i\n", minutos(180000));
+	printf("segundos restantes: %i\n", segundos(180000));
 	return 0;
 }
 
@@ -12,3 +15,34 @@ int days (int a) {
 	printf("Son %i dias\n", dias);
 	return a - dias*b;
 }
+
+int horas (int a) {
+	int b = 3600;
+	int horas = a / b;
+
+	printf("Son %i horas\n", horas);
+	return a - horas*b;
+}
+
+int minutos (int a) {
+	int b = 60;
+	int minutos = a / b;
+
+	printf("Son %i minutos\n", minutos);
+	return a - minutos*b;
+}
+
+
+int segundos (int a) {
+	int b = 1;
+	int segundos = a / b;
+
+	printf("Son %i segundos\n", segundos);
+	return a - segundos*b;
+}
+
+
+
+int total (int a) {
+	
+
